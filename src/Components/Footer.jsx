eb47ca1ad634f7beee14d715ad9b25973bcadae1
Shubhankar Singh Sisodia <shubhankarsinghsisodia@Shubhankars-MacBook-Pro.local>
2024-10-23 20:17:35 +0530

    changeInFooter

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -7,7 +7,7 @@ export default function Footer(){
         <footer>
         <div className="flex justify-center ">
         <div className="bottom-0 absolute w-full h-40 m-4  bg-customBlue  border-4 rounded-2xl">
-            footer element
+            footer element 2
         </div>
         </div>
         </footer>
